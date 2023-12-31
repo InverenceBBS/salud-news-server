3f4201e8688fe598a429a70ea43adfe7e6abaf20
StirlingSmith <stirlingjamessmith@gmail.com>
2024-01-01 08:53:00 +1300

    format from vec of dicts to df

--- src/news_collection/format_result.jl
+++ src/news_collection/format_result.jl
@@ -1,9 +1,14 @@
 """
-A function to format the resulting news articles. As yet this is just a placeholder in case we need it.
+A function to format the resulting news articles. Takes the vector of dictionaries and converts them into a DataFrame.
 
 # Arguments
 - `articles::Dict`: Dictionary from the `query_newsapi` function from `IOM_newsAPI_query`.
 """
 function format_result(articles)
-    return articles
+    dict_to_df(d) = DataFrame([Symbol(k)=>JSON.json(v) for (k,v) in d]...)
+
+    dfs = dict_to_df.(articles)
+
+    big_df = vcat(dfs...)
+    return big_df
 end
