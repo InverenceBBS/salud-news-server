e44b459b1c646b74fefacbe64ff92cb7b3776edb
StirlingSmith <stirlingjamessmith@gmail.com>
2024-01-01 08:53:27 +1300

    placeholder user to query news api

--- src/news_collection/read_user_file.jl
+++ src/news_collection/read_user_file.jl
@@ -7,6 +7,14 @@ NOTE: This function will likely change as we develop infrastructure for Salud.
 - `user_address::Stirng`: Address to read the user file at.
 """
 function read_user_file(user_address)
-    str = some_read(user_address)
-    JSON.parse(str)
+    # str = some_read(user_address)
+    # JSON.parse(str)
+    user = Dict("id"=>999, 
+            "keywords"=>Dict(
+                "keywords"=>["Sistema Nacional de Salud"], 
+                "locations"=>"https://en.wikipedia.org/wiki/Spain", 
+                "languages"=>"spa",
+                #"concepts"=>["https://es.wikipedia.org/wiki/Sistema_Nacional_de_Salud_(España)"],
+                "sources"=>nothing
+            ))
 end
