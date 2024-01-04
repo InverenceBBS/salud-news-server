36d97ca2371f069b9a6f79c4cac32ec791f13c93
StirlingSmith <stirlingjamessmith@gmail.com>
2024-01-05 11:51:37 +1300

    include functions

--- daily_job.jl
+++ daily_job.jl
@@ -1,1 +1,23 @@
+using Oxygen
+using SwaggerMarkdown
+using HTTP
+using Dates
+# think about adding Umbrella for authentication
+#using Umbrella
+using IOM_newsAPI_query
+using LibPQ
+using DataFrames
+using JSON
+using CSV
+using TimeZones
+
+
+#  Variable  Type  Description  Example 
+#  --------  ----  ------------------  --- 
+include("src/news_collection/format_result.jl")
+include("src/news_collection/read_user_file.jl")
+include("src/news_collection/write_formatted.jl")
+include("src/news_collection/db_conn.jl")
+
+include("src/collect_news_job.jl")
 collect_news_job("","")
