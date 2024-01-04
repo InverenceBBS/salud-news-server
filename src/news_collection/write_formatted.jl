3d4533ec0813c5f16beecacb21a6ef9704125012
StirlingSmith <stirlingjamessmith@gmail.com>
2024-01-05 11:27:33 +1300

    handle overwriting dupe keys

--- src/news_collection/write_formatted.jl
+++ src/news_collection/write_formatted.jl
@@ -43,7 +43,31 @@ function write_formatted(formatted, storage_address)
                 categories = formatted.concepts 
             ),
             conn,
-            "INSERT INTO $(ENV["HEALTHNEWSTABLENAME"]) (uri, url, time, dataType, eventUri, shares, location, wgt, isDuplicate, sim, body, image, sentiment, date, relevance, dateTimePub, source, lang, title, dateTime, authors, categories ) VALUES (\$1, \$2, \$3, \$4, \$5, \$6, \$7, \$8, \$9, \$10, \$11, \$12, \$13, \$14, \$15, \$16, \$17, \$18, \$19, \$20, \$21, \$22);"
+            "INSERT INTO $(ENV["HEALTHNEWSTABLENAME"]) (uri, url, time, dataType, eventUri, shares, location, wgt, isDuplicate, sim, body, image, sentiment, date, relevance, dateTimePub, source, lang, title, dateTime, authors, categories ) VALUES (\$1, \$2, \$3, \$4, \$5, \$6, \$7, \$8, \$9, \$10, \$11, \$12, \$13, \$14, \$15, \$16, \$17, \$18, \$19, \$20, \$21, \$22)
+            ON CONFLICT (uri) DO UPDATE SET
+                wgt = EXCLUDED.wgt,
+                uri = EXCLUDED.uri,
+                url = EXCLUDED.url ,
+                time = EXCLUDED.time,
+                dataType = EXCLUDED.dataType,  
+                eventUri = EXCLUDED.eventUri, 
+                shares = EXCLUDED.shares,
+                location = EXCLUDED.location, 
+                isDuplicate = EXCLUDED.isDuplicate,  
+                sim = EXCLUDED.sim,
+                body = EXCLUDED.body,  
+                image = EXCLUDED.image,   
+                sentiment = EXCLUDED.sentiment,
+                date = EXCLUDED.date,
+                relevance = EXCLUDED.relevance,
+                dateTimePub = EXCLUDED.dateTimePub,  
+                source = EXCLUDED.source,
+                lang = EXCLUDED.lang,  
+                title = EXCLUDED.title,  
+                dateTime = EXCLUDED.dateTime,
+                authors = EXCLUDED.authors,
+                categories = EXCLUDED.categories 
+            ;"
         );
 
         execute(conn, "COMMIT;")
