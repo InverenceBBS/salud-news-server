0de46be8c0c8d4ed5a9f67f5379ec5945b2eeafe
StirlingSmith <stirlingjamessmith@gmail.com>
2024-01-11 08:45:42 +1300

    adds concepts to user

--- src/news_collection/read_user_file.jl
+++ src/news_collection/read_user_file.jl
@@ -7,17 +7,21 @@ NOTE: This function will likely change as we develop infrastructure for Salud.
 - `user_address::Stirng`: Address to read the user file at.
 """
 function read_user_file(user_address)
-    # str = some_read(user_address)
-    # JSON.parse(str)
-    body = String(HTTP.get(ENV["HEALTHMETADATAADDR"]).body)
-    json = JSON.parse(body)
+
+    news_kws = String(HTTP.get(string(ENV["HEALTHMETADATAADDR"],"/news_kws")).body)
+    json = JSON.parse(news_kws)
     kws = getindex.(json, ["value"])
+
+    news_concepts = String(HTTP.get(string(ENV["HEALTHMETADATAADDR"],"/news_concept")).body)
+    json = JSON.parse(news_concepts)
+    concepts = getindex.(json, ["value"])
+
     user = Dict("id"=>999, 
             "keywords"=>Dict(
                 "keywords"=> kws, 
                 "locations"=>"https://en.wikipedia.org/wiki/Spain", 
                 "languages"=>nothing,
-                #"concepts"=>["https://es.wikipedia.org/wiki/Sistema_Nacional_de_Salud_(España)"],
+                "concepts"=>concepts,
                 "sources"=>nothing
             ))
 end
